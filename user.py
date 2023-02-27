933363749ee12252f75b32e446e4539a6a22a219
Yusuf <yusufbilgin1905@gmail.com>
2023-02-27 23:14:32 +0300

    init

---
+++ user.py
@@ -0,0 +1,13 @@
+
+class User():
+    def __init__(self, userID, userName, password, phone, email):
+        self.userID = userID
+        self.password = password
+        self.userName = userName
+        self.phone = phone
+        self.email = email
+
+
+
+
+
